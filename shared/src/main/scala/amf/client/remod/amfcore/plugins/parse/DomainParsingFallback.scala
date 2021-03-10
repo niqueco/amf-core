9687fa9f13b2e901858551be86ba8a7c36d5ddad
Agustin Bettati <bettatiagustin@gmail.com>
2021-03-10 13:15:00 -0300

    APIMF-2880: (MOD) - added UnsupportedVendorException in fallback implementation to maintain compatibility

--- shared/src/main/scala/amf/client/remod/amfcore/plugins/parse/DomainParsingFallback.scala
+++ shared/src/main/scala/amf/client/remod/amfcore/plugins/parse/DomainParsingFallback.scala
@@ -1,5 +1,6 @@
 package amf.client.remod.amfcore.plugins.parse
 
+import amf.core.exception.UnsupportedVendorException
 import amf.core.model.document.{BaseUnit, ExternalFragment}
 import amf.core.model.domain.ExternalDomainElement
 
@@ -10,13 +11,19 @@ private[amf] trait DomainParsingFallback {
 
 object ExternalFragmentDomainFallback extends DomainParsingFallback {
   override def chooseFallback(element: ParsingInfo, availablePlugins: Seq[AMFParsePlugin]): BaseUnit = {
-    val document = element.parsed
-    ExternalFragment()
-      .withId(document.location)
-      .withLocation(document.location)
-      .withEncodes(
-        ExternalDomainElement()
-          .withRaw(document.raw)
-          .withMediaType(document.mediatype))
+    element.vendor match {
+      case Some(definedVendor) =>
+        throw new UnsupportedVendorException(definedVendor)
+      case None =>
+        val document = element.parsed
+        ExternalFragment()
+          .withId(document.location)
+          .withLocation(document.location)
+          .withEncodes(
+            ExternalDomainElement()
+              .withRaw(document.raw)
+              .withMediaType(document.mediatype))
+    }
+
   }
 }
