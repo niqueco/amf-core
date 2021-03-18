d5d7f67986a2ac3c6c0c1736eaeef768a4be8cac
Tomas Fernandez <tfernandez@mulesoft.com>
2021-03-18 10:01:40 -0300

    (MOD) APIMF-2881: forced legacy AMFValidationPlugin to provide the remod plugins that should be used for validation. This is so AML and custom validations extended from a known profile weren't mixed up.

--- shared/src/main/scala/amf/client/plugins/AMFValidationPlugin.scala
+++ shared/src/main/scala/amf/client/plugins/AMFValidationPlugin.scala
@@ -2,14 +2,16 @@ package amf.client.plugins
 
 import amf.ProfileName
 import amf.client.execution.BaseExecutionEnvironment
+import amf.client.remod.amfcore.plugins.validate.AMFValidatePlugin
 import amf.core.model.document.BaseUnit
 import amf.core.remote.Platform
+import amf.core.resolution.pipelines.ResolutionPipeline
 import amf.core.unsafe.PlatformSecrets
 import amf.core.validation.core.ValidationProfile
 import amf.core.validation.{AMFValidationReport, EffectiveValidations}
 import amf.internal.environment.Environment
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 trait AMFValidationPlugin extends AMFPlugin with PlatformSecrets {
 
@@ -18,15 +20,5 @@ trait AMFValidationPlugin extends AMFPlugin with PlatformSecrets {
     */
   def domainValidationProfiles(platform: Platform): Map[String, () => ValidationProfile]
 
-  /**
-    * Request for validation of a particular model, profile and list of effective validations form that profile
-    */
-  def validationRequest(
-      baseUnit: BaseUnit,
-      profile: ProfileName,
-      validations: EffectiveValidations,
-      platform: Platform,
-      env: Environment,
-      resolved: Boolean = false,
-      executionEnv: BaseExecutionEnvironment = platform.defaultExecutionEnvironment): Future[AMFValidationReport]
+  protected[amf] def getRemodValidatePlugins(): Seq[AMFValidatePlugin]
 }
