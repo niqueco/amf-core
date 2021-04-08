e4cf837eac0d5b2f1c962bed36983e413cb2e2ba
llibarona <llibarona@salesforce.com>
2021-04-08 08:25:52 -0300

    For , the field annotation is inferred, leaving lexical info to the array itself

--- shared/src/main/scala/amf/core/model/domain/AmfObject.scala
+++ shared/src/main/scala/amf/core/model/domain/AmfObject.scala
@@ -55,7 +55,8 @@ trait AmfObject extends AmfElement {
   /** Set scalar value. */
   def set(field: Field, value: Boolean): this.type = set(field, AmfScalar(value))
 
-  def set(field: Field, value: Boolean, annotations: Annotations): this.type = set(field, AmfScalar(value), annotations)
+  def set(field: Field, value: Boolean, annotations: Annotations): this.type =
+    set(field, AmfScalar(value), annotations)
 
   /** Set scalar value. */
   def set(field: Field, value: Int): this.type = set(field, AmfScalar(value))
@@ -88,7 +89,7 @@ trait AmfObject extends AmfElement {
 
   /** Set field value. */
   def setArray(field: Field, values: Seq[AmfElement], annotations: Annotations): this.type = {
-    fields.set(id, field, AmfArray(values, annotations), annotations)
+    fields.set(id, field, AmfArray(values, annotations), Annotations.inferred())
     this
   }
 
@@ -100,7 +101,7 @@ trait AmfObject extends AmfElement {
 
   /** Set field value. */
   def setArrayWithoutId(field: Field, values: Seq[AmfElement], annotations: Annotations): this.type = {
-    fields.setWithoutId(field, AmfArray(values, annotations), annotations)
+    fields.setWithoutId(field, AmfArray(values, annotations), Annotations.inferred())
     this
   }
 
