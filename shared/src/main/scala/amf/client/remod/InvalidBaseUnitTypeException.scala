8ddbb6bf092ad225dac5d3f23b0fb6db624a2912
Hernan Najles <hernan.naj+github@gmail.com>
2021-04-14 16:33:21 -0300

    Add constructor for unit exception, so it can be build from Objs

--- shared/src/main/scala/amf/client/remod/InvalidBaseUnitTypeException.scala
+++ shared/src/main/scala/amf/client/remod/InvalidBaseUnitTypeException.scala
@@ -1,5 +1,12 @@
 package amf.client.remod
 
-case class InvalidBaseUnitTypeException(currentIri:String, exceptedIri:String) extends Exception(s"Current base unit of type $currentIri is not of expected type $exceptedIri"){
+import amf.core.metamodel.Obj
 
+case class InvalidBaseUnitTypeException(currentIri: String, exceptedIri: String)
+    extends Exception(s"Current base unit of type $currentIri is not of expected type $exceptedIri") {}
+
+object InvalidBaseUnitTypeException {
+  def forMeta(current: Obj, expected: Obj): InvalidBaseUnitTypeException = {
+    InvalidBaseUnitTypeException(current.`type`.headOption.map(_.iri()).getOrElse(""), expected.`type`.head.iri())
+  }
 }
