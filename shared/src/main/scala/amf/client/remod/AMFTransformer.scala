e18c8d8a673bbae89c5b09b49aa4aee502fa73cd
Agustin Bettati <bettatiagustin@gmail.com>
2021-05-20 16:02:15 -0300

    implement AMFTransformer transform method

--- shared/src/main/scala/amf/client/remod/AMFTransformer.scala
+++ shared/src/main/scala/amf/client/remod/AMFTransformer.scala
@@ -1,33 +1,34 @@
 package amf.client.remod
 
 import amf.core.model.document.BaseUnit
+import amf.core.resolution.pipelines.TransformationPipelineRunner
+import amf.core.validation.AMFValidationReport
+import amf.plugins.features.validation.CoreValidations.ResolutionValidation
 
 object AMFTransformer {
 
   def transform(unit: BaseUnit, configuration: AMFGraphConfiguration): AMFResult = ???
 
-  def transform(unit: BaseUnit, pipelineName: String, configuration: AMFGraphConfiguration): AMFResult = ???
-//  {
-//    val pipelines = conf.registry.transformationPipelines
-//    val pipeline  = pipelines.get(pipelineName)
-//
-//    val handler = DefaultErrorHandler() // what about the error handler provider?
-//    pipeline match {
-//      case Some(pipeline) =>
-//        val runner = TransformationPipelineRunner(handler, conf.listeners.toList)
-//        val result = runner.run(unit, pipeline)
-//        AMFResult(result, handler.getErrors)
-//      case None =>
-//        handler.violation(
-//          ResolutionValidation,
-//          unit.id,
-//          None,
-//          s"Cannot find transformation pipeline with name $pipelineName",
-//          unit.position(),
-//          unit.location()
-//        )
-//        unit
-//    }
-//  }
+  def transform(unit: BaseUnit, pipelineName: String, conf: AMFGraphConfiguration): AMFResult = {
+    val pipelines = conf.registry.transformationPipelines
+    val pipeline  = pipelines.get(pipelineName)
+    val handler   = conf.errorHandlerProvider.errorHandler()
+    val resolved = pipeline match {
+      case Some(pipeline) =>
+        val runner = TransformationPipelineRunner(handler, conf.listeners.toList)
+        runner.run(unit, pipeline)
+      case None =>
+        handler.violation(
+            ResolutionValidation,
+            unit.id,
+            None,
+            s"Cannot find transformation pipeline with name $pipelineName",
+            unit.position(),
+            unit.location()
+        )
+        unit
+    }
+    AMFResult(resolved, AMFValidationReport.forModel(resolved, handler.getResults))
+  }
 
 }
