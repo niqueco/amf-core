141eba7382e150f42a50692eafe7f993d28abddf
Agustin Bettati <bettatiagustin@gmail.com>
2021-05-31 16:34:53 -0300

    avoid returning results in static IgnoringErrorHandler

--- shared/src/main/scala/amf/client/parse/IgnoringErrorHandler.scala
+++ shared/src/main/scala/amf/client/parse/IgnoringErrorHandler.scala
@@ -1,6 +1,7 @@
 package amf.client.parse
 import amf.core.annotations.LexicalInformation
 import amf.core.errorhandling.AMFErrorHandler
+import amf.core.validation.AMFValidationResult
 import amf.core.validation.core.ValidationSpecification
 import org.mulesoft.lexer.SourceLocation
 import org.yaml.model.{SyamlException, YError}
@@ -14,4 +15,6 @@ object IgnoringErrorHandler extends AMFErrorHandler {
                        lexical: Option[LexicalInformation],
                        location: Option[String]): Unit              = {}
   override def handle(loc: SourceLocation, e: SyamlException): Unit = {}
+  override def report(result: AMFValidationResult): Unit            = {}
+  override def getResults: List[AMFValidationResult]                = Nil
 }
