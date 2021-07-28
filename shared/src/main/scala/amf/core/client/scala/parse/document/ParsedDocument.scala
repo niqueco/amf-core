1b73b5cfefa8f44360c91738a9706336ddb27b47
Antonio Garrote <antoniogarrote@gmail.com>
2021-07-28 21:18:23 +0200

    Removing ANTLR dependency

--- shared/src/main/scala/amf/core/client/scala/parse/document/ParsedDocument.scala
+++ shared/src/main/scala/amf/core/client/scala/parse/document/ParsedDocument.scala
@@ -1,7 +1,6 @@
 package amf.core.client.scala.parse.document
 
 import amf.core.internal.plugins.syntax.SourceCodeBlock
-import org.mulesoft.antlrast.ast.AST
 import org.yaml.model.YDocument
 
 
@@ -12,5 +11,3 @@ abstract class ParsedDocument {
 case class SyamlParsedDocument(document: YDocument, override val comment: Option[String] = None) extends ParsedDocument
 
 case class StringParsedDocument(ast: SourceCodeBlock, override val comment: Option[String] = None) extends ParsedDocument
-
-case class AntlrParsedDocument(ast: AST, override val comment: Option[String] = None) extends ParsedDocument
