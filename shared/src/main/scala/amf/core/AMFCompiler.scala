33c69855f9351fb90accb6e668c64c80ec46c616
Agustin Bettati <bettatiagustin@gmail.com>
2022-01-20 11:39:00 -0300

    APIMF-3600: remove normalize from Root location

--- shared/src/main/scala/amf/core/AMFCompiler.scala
+++ shared/src/main/scala/amf/core/AMFCompiler.scala
@@ -405,13 +405,3 @@ case class Root(parsed: ParsedDocument,
                 references: Seq[ParsedReference],
                 referenceKind: ReferenceKind,
                 raw: String) {}
-
-object Root {
-  def apply(parsed: ParsedDocument,
-            location: String,
-            mediatype: String,
-            references: Seq[ParsedReference],
-            referenceKind: ReferenceKind,
-            raw: String): Root =
-    new Root(parsed, location.normalizeUrl, mediatype, references, referenceKind, raw)
-}
