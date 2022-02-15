2ffe1790768832821b28fe1702233d086b98d7b8
Nicolas Schejtman <nschejtman93@gmail.com>
2022-02-15 10:35:48 -0300

    Add some TODO comments with required refactors

--- shared/src/main/scala/amf/core/client/scala/traversal/ModelTraversalRegistry.scala
+++ shared/src/main/scala/amf/core/client/scala/traversal/ModelTraversalRegistry.scala
@@ -10,6 +10,14 @@ class ModelTraversalRegistry() {
   // IDs visited in the current path
   private[amf] var currentPath: Set[String] = Set.empty
 
+  /***
+    * TODO (Refactor needed): Traversal stack management
+    * Responsibility for managing the traversal stack is split across the ModelTraversalRegistry is its callers. Callers
+    * push elements onto the stack by calling the `+` method. The ModelTraversalRegistry pops elements from the stack
+    * with `runNested`, which also executes a function self.type => T that eventually pushes more elements onto the stack.
+    * Stack management is a complex tandem between the ModelTraversalRegistry and its callers which should be simplified
+    * to better debug traversals.
+    */
   def +(id: String): this.type = {
     visitedIds += id
     currentPath += id
