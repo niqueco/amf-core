4eea5b36c261c55ad812b883e897736335b1b155
Nicolas Schejtman <nschejtman93@gmail.com>
2022-02-15 22:27:27 -0300

    Remove allowCycleClasses from ShapeTraversalRegistry

--- shared/src/main/scala/amf/core/client/scala/traversal/ShapeTraversalRegistry.scala
+++ shared/src/main/scala/amf/core/client/scala/traversal/ShapeTraversalRegistry.scala
@@ -9,15 +9,6 @@ case class ShapeTraversalRegistry() extends ModelTraversalRegistry() {
 
   def isAllowListed(id: String): Boolean = allowList.contains(id)
 
-  private var allowedCycleClasses: Seq[Class[_]] = Seq()
-
-  def withAllowedCyclesInstances(classes: Seq[Class[_]]): this.type = {
-    allowedCycleClasses = classes
-    this
-  }
-
-  def isAllowedToCycle(shape: Shape): Boolean = allowedCycleClasses.contains(shape.getClass)
-
   def allow(shapeIds: Set[String])(fnc: () => Shape): Shape = {
     val previousAllowList = allowList
     allowList = allowList ++ shapeIds
@@ -27,11 +18,8 @@ case class ShapeTraversalRegistry() extends ModelTraversalRegistry() {
   }
 
   def foundRecursion(root: Shape, current: Shape): Boolean = root.annotations.find(classOf[TypeAlias]) match {
-    case Some(alias) =>
-      val a = alias.aliasId.equals(current.id) && currentPath.nonEmpty || isInCurrentPath(current.id)
-      val b = !isAllowedToCycle(current)
-      a && b
-    case None => isInCurrentPath(current.id) && !isAllowedToCycle(current)
+    case Some(alias) => alias.aliasId.equals(current.id) && currentPath.nonEmpty || isInCurrentPath(current.id)
+    case None        => isInCurrentPath(current.id)
   }
 
 }
