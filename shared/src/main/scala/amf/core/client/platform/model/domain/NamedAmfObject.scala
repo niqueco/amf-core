7f027e37436b39ed054a965816fb6f1fa16766b4
Loose <alejandro.loose@mulesoft.com>
2022-08-31 12:45:38 -0300

    W-11558453, W-11558514: required changes for new ComponentModule in amf

--- shared/src/main/scala/amf/core/client/platform/model/domain/NamedAmfObject.scala
+++ shared/src/main/scala/amf/core/client/platform/model/domain/NamedAmfObject.scala
@@ -2,14 +2,14 @@ package amf.core.client.platform.model.domain
 
 import amf.core.client.platform.model.StrField
 
-/** All DomainElements supporting name
+/** All AmfObject supporting name
   */
-trait NamedDomainElement {
+trait NamedAmfObject {
 
-  /** Return DomainElement name. */
+  /** Return AmfObject name. */
   def name: StrField
 
-  /** Update DomainElement name. */
+  /** Update AmfObject name. */
   def withName(name: String): this.type
 
 }
