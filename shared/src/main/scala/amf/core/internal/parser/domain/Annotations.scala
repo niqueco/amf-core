050292343cfd2a0d398b2384e7bb2815e055a093
Hernán Najles <50325041+pope1838@users.noreply.github.com>
2022-10-04 15:56:50 -0300

    Fix test due to bad cherry pick (#542)

--- shared/src/main/scala/amf/core/internal/parser/domain/Annotations.scala
+++ shared/src/main/scala/amf/core/internal/parser/domain/Annotations.scala
@@ -117,6 +117,8 @@ class Annotations() {
 
   // This should be in amf-shapes BUT PropertyShape is in amf-core
   def isInferredProperty: Boolean = find(classOf[InferredProperty]).isDefined
+
+  def clear() = annotations.clear()
 }
 
 object Annotations {
@@ -130,8 +132,9 @@ object Annotations {
   }
 
   def apply(ast: YPart): Annotations = {
-    val annotations = new Annotations() ++= Set(LexicalInformation(ast),
-                                                SourceYPart(ast),
+    val annotations = new Annotations() ++= Set(
+        LexicalInformation(ast),
+        SourceYPart(ast),
 //      AmfSourceLocation(ast.sourceName))
         AmfSourceLocation(ast)
     )
