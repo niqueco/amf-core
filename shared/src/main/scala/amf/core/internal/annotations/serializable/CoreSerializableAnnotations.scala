4b74e499ee110c022061bbc7f36d7855735325a2
hghianni <hghianni@mulesoft.com>
2022-10-05 10:54:03 -0300

    W-11350149: Add virtual element to list of serializable annotations

--- shared/src/main/scala/amf/core/internal/annotations/serializable/CoreSerializableAnnotations.scala
+++ shared/src/main/scala/amf/core/internal/annotations/serializable/CoreSerializableAnnotations.scala
@@ -6,16 +6,17 @@ import amf.core.client.scala.model.domain.AnnotationGraphLoader
 private[amf] object CoreSerializableAnnotations extends SerializableAnnotations {
 
   override val annotations: Map[String, AnnotationGraphLoader] = Map(
-      "lexical"              -> LexicalInformation,
-      "host-lexical"         -> HostLexicalInformation,
-      "base-path-lexical"    -> BasePathLexicalInformation,
-      "single-value-array"   -> SingleValueArray,
-      "aliases-array"        -> Aliases,
-      "synthesized-field"    -> SynthesizedField,
-      "default-node"         -> DefaultNode,
-      "data-node-properties" -> DataNodePropertiesAnnotations,
-      "resolved-link"        -> ResolvedLinkAnnotation,
-      "null-security"        -> NullSecurity
+    "lexical"              -> LexicalInformation,
+    "host-lexical"         -> HostLexicalInformation,
+    "base-path-lexical"    -> BasePathLexicalInformation,
+    "single-value-array"   -> SingleValueArray,
+    "aliases-array"        -> Aliases,
+    "synthesized-field"    -> SynthesizedField,
+    "default-node"         -> DefaultNode,
+    "data-node-properties" -> DataNodePropertiesAnnotations,
+    "resolved-link"        -> ResolvedLinkAnnotation,
+    "null-security"        -> NullSecurity,
+    "virtual-element"      -> VirtualElement
   )
 
 }
